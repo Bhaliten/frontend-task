456278e2cc497c6d493e95a899e8123014c03269
simon <bhaliten@gmail.com>
2024-04-18 10:56:03 +0200

    yes

--- src/app/components/user-create-form/user-create-form.component.ts
+++ src/app/components/user-create-form/user-create-form.component.ts
@@ -1,18 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ChipsModule} from "primeng/chips";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {UserHandlerService} from "../../services/user-handler.service";
+import {User} from "../../models/user";
+import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-user-create-form',
-  standalone: true,
+    selector: 'app-user-create-form',
+    standalone: true,
     imports: [
         ChipsModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule,
+        NgIf
     ],
-  templateUrl: './user-create-form.component.html',
-  styleUrl: './user-create-form.component.scss'
+    templateUrl: './user-create-form.component.html',
+    styleUrl: './user-create-form.component.scss'
 })
 export class UserCreateFormComponent {
-    value: string | undefined;
 
+    constructor(private userHandlerService: UserHandlerService) {
+    }
+
+    createUserForm = new FormGroup({
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl(''),
+    });
+
+    addNewUser() {
+        // const user = new User(
+        //     "",
+        //     this.createUserForm.getRawValue().firstName,
+        //     this.createUserForm.getRawValue().lastName,
+        //     this.createUserForm.getRawValue().email
+        // );
+        if (this.createUserForm.valid){
+            let user: User = this.createUserForm.value as User;
+            this.userHandlerService.testData.push(user);
+        }
+
+    }
 }
